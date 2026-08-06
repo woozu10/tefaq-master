9ad258fdc09fb3ece75ee81ce0add8e808c8df2b
woozu10 <woozu10@gmail.com>
2026-08-06 18:09:03 -0400

    Add Dashboard cards

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,25 @@
 export function Content() {
   return `
     <main class="content">
-      Welcome to TEFAQ Master
+
+      <h2>Dashboard</h2>
+
+      <div class="card">
+        Listening Progress
+      </div>
+
+      <div class="card">
+        Reading Progress
+      </div>
+
+      <div class="card">
+        Speaking Progress
+      </div>
+
+      <div class="card">
+        Writing Progress
+      </div>
+
     </main>
   `;
 }
