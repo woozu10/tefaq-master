741d671cf4be13b3410754cb2494e005bf9229d4
woozu10 <woozu10@gmail.com>
2026-08-06 18:06:32 -0400

    Add Navigation to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
+import { Navigation } from "./Navigation.js";
+
 export function Header() {
   return `
     <header class="header">
       <h2>TEFAQ Master</h2>
+      ${Navigation()}
     </header>
   `;
 }
