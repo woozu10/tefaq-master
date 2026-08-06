6bc8b6d9e73a33d693027ef32fab18503dea7ce9
woozu10 <woozu10@gmail.com>
2026-08-06 17:41:20 -0400

    Add Navigation component

---
+++ src/components/Navigation.js
@@ -0,0 +1,11 @@
+export function Navigation() {
+  return `
+    <nav>
+      <button>Dashboard</button>
+      <button>CO</button>
+      <button>CE</button>
+      <button>EO</button>
+      <button>EE</button>
+    </nav>
+  `;
+}
