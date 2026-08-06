e13ae3b1314943788e04d07e928a4ad900cd5456
woozu10 <woozu10@gmail.com>
2026-08-06 17:42:26 -0400

    Add Navigation to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { Navigation } from "./components/Navigation.js";
 import { Dashboard } from "./pages/Dashboard.js";
 
 export function App() {
   return `
     <h1>TEFAQ Master</h1>
+    ${Navigation()}
     ${Dashboard()}
   `;
 }
