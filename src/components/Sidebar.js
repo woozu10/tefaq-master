6fec6ae47aed2d05d2700e855159552fcb25c87e
woozu10 <woozu10@gmail.com>
2026-08-06 17:56:38 -0400

    Add Sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,13 @@
+export function Sidebar() {
+  return `
+    <aside class="sidebar">
+      <ul>
+        <li>Dashboard</li>
+        <li>Listening</li>
+        <li>Reading</li>
+        <li>Writing</li>
+        <li>Speaking</li>
+      </ul>
+    </aside>
+  `;
+}
