d76d31c1af39be9585e473c71ceb96f19f6bbeeb
woozu10 <woozu10@gmail.com>
2026-08-06 22:02:30 -0400

    Update CO.js

--- src/pages/CO.js
+++ src/pages/CO.js
@@ -1,3 +1,4 @@
+let selected = -1;
 import { coQuestions } from "../data/coQuestions.js";
 
 export function CO(){
@@ -14,16 +15,38 @@ export function CO(){
 
             <h3>${q.question}</h3>
 
-            <button>${q.choices[0]}</button>
+<button onclick="checkAnswer(1)">
+${q.choices[1]}
+</button>
 
-            <button>${q.choices[1]}</button>
+<button onclick="checkAnswer(2)">
+${q.choices[2]}
+</button>
 
-            <button>${q.choices[2]}</button>
+<button onclick="checkAnswer(3)">
+${q.choices[3]}
+</button>
 
         </div>
 
     </main>
 
     `;
+
+}
+
+window.checkAnswer = function(index){
+
+    const q = coQuestions[0];
+
+    if(index===q.answer){
+
+        alert("✅ Correct!");
+
+    }else{
+
+        alert("❌ Incorrect");
+
+    }
 
 }
