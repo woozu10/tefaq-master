f94edebf9c64540409272f393ce4c057aae24193
woozu10 <woozu10@gmail.com>
2026-08-07 00:24:08 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import { Dashboard } from "./pages/Dashboard.js";
+import { COHome } from "./pages/COHome.js";
 import { CO } from "./pages/CO.js";
 import { Result } from "./pages/Result.js";
 import { getCurrentPage } from "./services/router.js";
 
 export function App() {
 
-    const page = getCurrentPage();
+  const page = getCurrentPage();
 
-    switch(page){
+  switch (page) {
 
-        case "co":
-            return CO();
+    case "co":
+      return COHome();
 
-        case "result":
+    case "co-practice":
+      return CO();
 
-            const score = Number(localStorage.getItem("score"));
-            const total = Number(localStorage.getItem("total"));
+    case "result":
+      const score = Number(localStorage.getItem("score"));
+      const total = Number(localStorage.getItem("total"));
+      return Result(score, total);
 
-            return Result(score,total);
+    default:
+      return Dashboard();
 
-        default:
-            return Dashboard();
-
-    }
+  }
 
 }
