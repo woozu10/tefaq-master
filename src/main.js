4fa3a089d3bc1713fd1ac7f3c2d65ea2c66416f0
woozu10 <woozu10@gmail.com>
2026-08-06 21:32:49 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import { App } from "./App.js";
+import { getCurrentPage } from "./services/router.js";
 
-document.getElementById("app").innerHTML = App();
+document.getElementById("app").innerHTML = App(getCurrentPage());
