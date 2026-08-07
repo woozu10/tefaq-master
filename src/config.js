6760a89c95e660101a9592c01a33e0e080b50ea6
woozu10 <woozu10@gmail.com>
2026-08-07 00:55:07 -0400

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,27 @@
+export const CONFIG = {
+
+  exam: {
+
+    time: 40,
+
+    questionCount: 20
+
+  },
+
+  practice: {
+
+    showAnswer: true,
+
+    random: true
+
+  },
+
+  app: {
+
+    title: "TEFAQ Master",
+
+    version: "0.1.0"
+
+  }
+
+};
