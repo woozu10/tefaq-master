54615570c62a16073c3476e5d59c581b479da358
woozu10 <woozu10@gmail.com>
2026-08-07 00:21:15 -0400

    Create COHome.js

---
+++ src/pages/COHome.js
@@ -0,0 +1,35 @@
+export function COHome() {
+
+  return `
+    <main class="content">
+
+      <h2>Compréhension Orale</h2>
+
+      <div class="card">
+
+        <button data-page="co-practice">
+          📘 Practice
+        </button>
+
+        <button data-page="co-exam">
+          📝 Exam
+        </button>
+
+        <button data-page="co-wrong">
+          ❌ Wrong Notes
+        </button>
+
+        <button data-page="co-favorite">
+          ⭐ Favorites
+        </button>
+
+        <button data-page="co-stat">
+          📊 Statistics
+        </button>
+
+      </div>
+
+    </main>
+  `;
+
+}
