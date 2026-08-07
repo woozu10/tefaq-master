13289231d5b243c76a0d47b99845c7b55307e262
woozu10 <woozu10@gmail.com>
2026-08-06 21:11:54 -0400

    Create CO.js

---
+++ src/pages/CO.js
@@ -0,0 +1,12 @@
+export function CO() {
+  return `
+    <main class="content">
+      <h2>Compréhension Orale</h2>
+
+      <div class="card">
+        <h3>Listening Practice</h3>
+        <p>Welcome to CO Practice.</p>
+      </div>
+    </main>
+  `;
+}
