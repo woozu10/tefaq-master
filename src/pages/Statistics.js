2e32d7009ebadd8652e91dc112d71d4b313f22b7
woozu10 <woozu10@gmail.com>
2026-08-07 07:11:03 -0400

    Update Statistics.js

--- src/pages/Statistics.js
+++ src/pages/Statistics.js
@@ -1,13 +1,59 @@
-import { getHistory } from "../services/statisticsService.js";
+import {
+  getHistory,
+  clearHistory
+} from "../services/statisticsService.js";
+
+window.clearStatistics = function () {
+
+  if (!confirm("Delete all statistics?")) {
+
+    return;
+
+  }
+
+  clearHistory();
+
+  location.reload();
+
+};
 
 export function Statistics() {
 
   const history = getHistory();
 
+  const average =
+    history.length === 0
+      ? 0
+      : Math.round(
+          history.reduce(
+            (sum, item) => sum + item.percent,
+            0
+          ) / history.length
+        );
+
   return `
+
     <main class="content">
 
       <h2>📊 Statistics</h2>
+
+      <div class="card">
+
+        <h3>Total Exams</h3>
+
+        <h1>${history.length}</h1>
+
+        <h3>Average</h3>
+
+        <h2>${average}%</h2>
+
+      </div>
+
+      <button onclick="clearStatistics()">
+        🗑 Clear History
+      </button>
+
+      <hr>
 
       ${
         history.length === 0
@@ -26,6 +72,7 @@ export function Statistics() {
       }
 
     </main>
+
   `;
 
 }
