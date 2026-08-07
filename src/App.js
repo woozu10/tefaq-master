0cdf4d02123d868ab50d5a554f1256f62f49ad18
woozu10 <woozu10@gmail.com>
2026-08-07 00:27:10 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Dashboard } from "./pages/Dashboard.js";
 import { COHome } from "./pages/COHome.js";
+import { COPractice } from "./pages/COPractice.js";
 import { CO } from "./pages/CO.js";
 import { Result } from "./pages/Result.js";
 import { getCurrentPage } from "./services/router.js";
@@ -14,12 +15,18 @@ export function App() {
       return COHome();
 
     case "co-practice":
+      return COPractice();
+
+    case "co-quiz":
       return CO();
 
-    case "result":
+    case "result": {
+
       const score = Number(localStorage.getItem("score"));
       const total = Number(localStorage.getItem("total"));
+
       return Result(score, total);
+    }
 
     default:
       return Dashboard();
