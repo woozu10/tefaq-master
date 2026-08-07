7fd383957ba9a4bf8b287edb6fe039129a9f8b24
woozu10 <woozu10@gmail.com>
2026-08-07 00:57:06 -0400

    Update AudioPlayer.js

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -3,33 +3,15 @@ export function AudioPlayer(audio) {
   return `
     <div class="audio-player">
 
-      <button id="play-btn" onclick="playAudio('${audio}')">
-        🔊 Play Audio
-      </button>
+      <audio controls preload="none">
 
-      <audio id="audio-player">
         <source src="${audio}" type="audio/mpeg">
+
+        Your browser does not support audio.
+
       </audio>
 
     </div>
   `;
-
-}
-
-window.playAudio = function(audio){
-
-  const player = document.getElementById("audio-player");
-  const button = document.getElementById("play-btn");
-
-  player.play();
-
-  button.disabled = true;
-  button.innerText = "▶ Playing...";
-
-  player.onended = () => {
-
-    button.innerText = "✔ Played";
-
-  };
 
 }
