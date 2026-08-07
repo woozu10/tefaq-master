9748aefd735c86d22a6f73746f32d065f785ba94
woozu10 <woozu10@gmail.com>
2026-08-07 02:05:31 -0400

    Create quizService.js

---
+++ src/services/quizService.js
@@ -0,0 +1,19 @@
+let quizQuestions = [];
+
+export function setQuizQuestions(questions) {
+
+  quizQuestions = questions;
+
+}
+
+export function getQuizQuestions() {
+
+  return quizQuestions;
+
+}
+
+export function clearQuizQuestions() {
+
+  quizQuestions = [];
+
+}
