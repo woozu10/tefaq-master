7c4cfaa980a6b959d2976f8d5c840cdab6a0ca86
woozu10 <woozu10@gmail.com>
2026-08-06 21:40:49 -0400

    Create coQuestions.js

---
+++ src/data/coQuestions.js
@@ -0,0 +1,20 @@
+export const coQuestions = [
+
+  {
+    id:1,
+    level:"B2",
+
+    audio:"",
+
+    question:"Pourquoi Marie téléphone-t-elle ?",
+
+    choices:[
+      "Pour réserver un hôtel",
+      "Pour annuler un rendez-vous",
+      "Pour demander un renseignement"
+    ],
+
+    answer:1
+  }
+
+];
