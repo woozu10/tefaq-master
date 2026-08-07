ea2cbf9f418fe80a3f725530984a570f3631ae71
woozu10 <woozu10@gmail.com>
2026-08-06 23:59:38 -0400

    Update AudioPlayer.js

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,17 +1,35 @@
 export function AudioPlayer(audio) {
 
   return `
-    <button onclick="playAudio('${audio}')">
-      🔊 Play Audio
-    </button>
+    <div class="audio-player">
+
+      <button id="play-btn" onclick="playAudio('${audio}')">
+        🔊 Play Audio
+      </button>
+
+      <audio id="audio-player">
+        <source src="${audio}" type="audio/mpeg">
+      </audio>
+
+    </div>
   `;
 
 }
 
 window.playAudio = function(audio){
 
-  const player = new Audio(audio);
+  const player = document.getElementById("audio-player");
+  const button = document.getElementById("play-btn");
 
   player.play();
+
+  button.disabled = true;
+  button.innerText = "▶ Playing...";
+
+  player.onended = () => {
+
+    button.innerText = "✔ Played";
+
+  };
 
 }
