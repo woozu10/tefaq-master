ecde2a63f76c0a86fe605b463c28f2023b08f5cf
woozu10 <woozu10@gmail.com>
2026-08-06 21:42:23 -0400

    Update CO.js

--- src/pages/CO.js
+++ src/pages/CO.js
@@ -1,12 +1,29 @@
-export function CO() {
-  return `
-    <main class="content">
-      <h2>Compréhension Orale</h2>
+import { coQuestions } from "../data/coQuestions.js";
 
-      <div class="card">
-        <h3>Listening Practice</h3>
-        <p>Welcome to CO Practice.</p>
-      </div>
+export function CO(){
+
+    const q=coQuestions[0];
+
+    return `
+
+    <main class="content">
+
+        <h2>Compréhension Orale</h2>
+
+        <div class="card">
+
+            <h3>${q.question}</h3>
+
+            <button>${q.choices[0]}</button>
+
+            <button>${q.choices[1]}</button>
+
+            <button>${q.choices[2]}</button>
+
+        </div>
+
     </main>
-  `;
+
+    `;
+
 }
