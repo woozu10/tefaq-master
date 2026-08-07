035949e7c0d94aa9920acfaec9fd50f8f45cde92
woozu10 <woozu10@gmail.com>
2026-08-06 21:24:59 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Dashboard } from "./pages/Dashboard.js";
 import { CO } from "./pages/CO.js";
+import { getCurrentPage } from "./services/router.js";
 
-export function App(page = "dashboard") {
+export function App() {
 
-  switch(page){
+  switch (getCurrentPage()) {
 
     case "co":
       return CO();
