8dd87fe265e790f716fdc6374006c766813465d1
woozu10 <woozu10@gmail.com>
2026-08-07 08:14:00 -0400

    Create reviewService.js

---
+++ src/services/reviewService.js
@@ -0,0 +1,13 @@
+let reviewQuestions = [];
+
+export function setReviewQuestions(questions) {
+
+  reviewQuestions = questions;
+
+}
+
+export function getReviewQuestions() {
+
+  return reviewQuestions;
+
+}
