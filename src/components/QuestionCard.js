92cae834b96b448293c5cbb0c10f5d4e820c2070
woozu10 <woozu10@gmail.com>
2026-08-06 23:32:47 -0400

    Update QuestionCard.js

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,25 +1,19 @@
-export function QuestionCard(question, choices) {
+export function QuestionCard(
+  question,
+  choices,
+  renderButton
+) {
 
   return `
-
     <div class="card">
 
       <h3>${question}</h3>
 
-      <button onclick="checkAnswer(0)">
-        ${choices[0]}
-      </button>
-
-      <button onclick="checkAnswer(1)">
-        ${choices[1]}
-      </button>
-
-      <button onclick="checkAnswer(2)">
-        ${choices[2]}
-      </button>
+      ${renderButton(0)}
+      ${renderButton(1)}
+      ${renderButton(2)}
 
     </div>
-
   `;
 
 }
