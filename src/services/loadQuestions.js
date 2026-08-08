3d450d5a3bb3f1848ab9b00f49a54e88548132ac
woozu10 <woozu10@gmail.com>
2026-08-08 03:23:02 -0400

    Update loadQuestions.js

--- src/services/loadQuestions.js
+++ src/services/loadQuestions.js
@@ -1,9 +1,14 @@
 export async function loadQuestions(level = "B2") {
 
-  const response = await fetch(`/data/co/${level}.json`);
+  const url =
+    `${import.meta.env.BASE_URL}data/co/${level}.json`;
+
+  const response = await fetch(url);
 
   if (!response.ok) {
-    throw new Error(`Cannot load ${level}.json`);
+    throw new Error(
+      `Cannot load ${level}.json (${response.status})`
+    );
   }
 
   return await response.json();
