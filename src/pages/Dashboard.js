c1c8f60ba7dc906e9ae9750c0757a2dad177efd1
woozu10 <woozu10@gmail.com>
2026-08-08 09:23:47 -0400

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,13 +1,50 @@
 import { Header } from "../components/Header.js";
 import { Sidebar } from "../components/Sidebar.js";
 import { Content } from "../components/Content.js";
+import { setCurrentPage } from "../services/router.js";
+
+window.openCO = function () {
+  setCurrentPage("co");
+  location.reload();
+};
+
+window.openCE = function () {
+  setCurrentPage("ce");
+  location.reload();
+};
 
 export function Dashboard() {
+
   return `
+
     ${Header()}
+
     <div class="layout">
+
       ${Sidebar()}
-      ${Content()}
+
+      <main class="content">
+
+        <h2>TEFAQ MASTER</h2>
+
+        <div class="card">
+
+          <button onclick="openCO()">
+            🎧 Compréhension Orale
+          </button>
+
+          <button onclick="openCE()">
+            📖 Compréhension Écrite
+          </button>
+
+        </div>
+
+        ${Content()}
+
+      </main>
+
     </div>
+
   `;
+
 }
