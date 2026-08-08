f44436412e283eda65727dd08262f108d6721108
woozu10 <woozu10@gmail.com>
2026-08-08 11:41:19 -0400

    Create EEExam.js

---
+++ src/pages/EEExam.js
@@ -0,0 +1,25 @@
+export function EEExam() {
+
+  return `
+
+    <main class="content">
+
+      <h2>Expression Écrite</h2>
+
+      <div class="card">
+
+        <h3>Exam Mode</h3>
+
+        <p>Time Limit : 30 minutes</p>
+
+        <button onclick="goPage('ee-quiz')">
+          Start Exam
+        </button>
+
+      </div>
+
+    </main>
+
+  `;
+
+}
